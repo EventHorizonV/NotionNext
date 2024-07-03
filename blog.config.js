da52b7b642239f2f8466b8789bf0b3312e92ca4a
事件视界 <57124916+EventHorizonV@users.noreply.github.com>
2024-07-03 18:05:41 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -313,7 +313,7 @@ const BLOG = {
       artist: '藤井风',
       url: 'https://music.163.com/song/media/outer/url?id=2090470651.mp3',
       cover:
-        'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/109951168978915269.jpg'
+        'https://p2.music.126.net/j-x3egNXCQWeTk3h6_WCsA==/109951168978915269.jpg'
     }
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
